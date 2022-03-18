426a969c2f931e93bf74f903b7bb1a0f3c5693f5
Adrian Brown <adrian.r.brown90@gmail.com>
2022-03-18 07:31:41 -0700

    add albums table update queries

---
+++ update_exercises.sql
@@ -0,0 +1,22 @@
+USE codeup_test_db;
+
+SELECT 'All albums in your table' AS 'Caption';
+SELECT * FROM albums;
+
+UPDATE albums
+SET sales = (sales * 10)
+WHERE sales > 0;
+
+SELECT 'All albums released before 1980' AS 'Caption';
+SELECT name FROM albums WHERE release_date < 1980;
+
+Update albums
+SET release_date = (release_date - 100);
+WHERE release_date < 1980;
+
+SELECT 'All albums by Michael Jackson' AS 'Caption';
+SELECT name FROM albums WHERE artist = 'Michael Jackson';
+
+Update albums
+SET artist = 'Peter Jackson';
+WHERE artist = 'Michael Jackson';
