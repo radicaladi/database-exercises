689d55771d7a1d0335b583257d51b3fd25c026c8
Adrian Brown <adrian.r.brown90@gmail.com>
2022-03-18 07:54:58 -0700

    add syntax error fix

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,5 +1,7 @@
 USE codeup_test_db;
 
+TRUNCATE albums;
+
 INSERT INTO albums (artist, name, release_date, sales, genre)
 VALUES ( 'Michael Jackson', 'Thriller', 1982, 49.2, 'Pop'),
        ( 'AC/DC', 'Back in Black', 1980, 29.5, 'Hard Rock'),
--- select_exercises.sql
+++ select_exercises.sql
@@ -1,19 +1,19 @@
 USE codeup_test_db;
 
 SELECT 'The name of all albums by Pink Floyd' AS 'Info';
-SELECT name FROM albums WHERE artist = 'Pink FLoyd'
+SELECT name FROM albums WHERE artist = 'Pink FLoyd';
 
 SELECT 'The year Sgt. Pepper''s Lonely Hearts Club Band was released' AS 'Info';
-SELECT release_date FROM album WHERE name = 'The year Sgt. Pepper''s Lonely Hearts Club Band was released';
+SELECT release_date FROM albums WHERE name = 'The year Sgt. Pepper''s Lonely Hearts Club Band was released';
 
 SELECT 'The genre for Nevermind' AS 'Info';
-SELECT genre FROM album WHERE artist = 'Nirvana';
+SELECT genre FROM albums WHERE artist = 'Nirvana';
 
 SELECT 'Which albums were released in the 1990s' AS 'Info';
-SELECT name FROM album WHERE release_date BETWEEN 1990 AND 2000;
+SELECT name FROM albums WHERE release_date BETWEEN 1990 AND 2000;
 
 SELECT 'Which albums had less than 20 million certified sales' AS 'Info';
-SELECT name FROM album WHERE sales < 20;
+SELECT name FROM albums WHERE sales < 20;
 
 SELECT 'All the albums with a genre of Rock' AS 'Info';
-SELECT name FROM album WHERE genre = 'Rock';
+SELECT name FROM albums WHERE genre = 'Rock';
