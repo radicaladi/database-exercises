105b8e9f573f118a61d2e1ae756195c75343770b
Adrian Brown <adrian.r.brown90@gmail.com>
2022-03-19 09:58:06 -0700

    add OR clause

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,8 +1,9 @@
 USE employees;
 
-SELECT first_name
+SELECT first_name, gender
 FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+WHERE first_name IN ('Irena', 'Vidya')
+   OR first_name = 'Maya';
 
 SELECT last_name
 FROM employees
