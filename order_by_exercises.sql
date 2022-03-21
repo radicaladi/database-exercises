ad5413b609fd168737d6adeb9fd1dd086eef06fa
Adrian Brown <adrian.r.brown90@gmail.com>
2022-03-21 07:40:38 -0700

    order by last_name, first_name -- irena, vidya, maya; order by emp_no

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -4,12 +4,13 @@ FROM employees
 WHERE first_name IN ('Irena', 'Vidya')
    OR first_name = 'Maya'
     AND gender = 'male'
-ORDER BY first_name, last_name;
+ORDER BY lastt_name, first_name;
 
-SELECT last_name
+SELECT last_name, emp_no
 FROM employees
 WHERE last_name LIKE 'e%'
-   OR last_name LIKE '%e';
+   OR last_name LIKE '%e'
+ORDER BY emp_no;
 
 SELECT last_name
 FROM employees
