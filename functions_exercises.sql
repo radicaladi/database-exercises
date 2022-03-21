ec15d21d0d5c91fcff3cd821c1d77ba147dfc2f4
Adrian Brown <adrian.r.brown90@gmail.com>
2022-03-21 09:38:15 -0700

    initial commit

---
+++ functions_exercises.sql
@@ -0,0 +1,24 @@
+USE employees;
+
+SELECT first_name, last_name, gender
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya')
+   OR first_name = 'Maya'
+    AND gender = 'male'
+ORDER BY last_name DESC, first_name DESC;
+
+SELECT last_name, emp_no
+FROM employees
+WHERE last_name LIKE 'e%'
+   OR last_name LIKE '%e'
+ORDER BY emp_no DESC;
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'e%'
+  AND last_name LIKE '%e';
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+  AND last_name NOT LIKE '%qu%';
