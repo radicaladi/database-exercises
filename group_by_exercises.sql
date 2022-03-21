aad3945cb2a19ff3815280237881542117c00dcd
Adrian Brown <adrian.r.brown90@gmail.com>
2022-03-21 13:22:38 -0700

    add distinct last_name query contains q but not qu

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -6,3 +6,8 @@ FROM titles;
 SELECT DISTINCT first_name, last_name
 FROM employees
 WHERE last_name LIKE 'e%e';
+
+SELECT DISTINCT last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+  AND last_name NOT LIKE '%qu%';
