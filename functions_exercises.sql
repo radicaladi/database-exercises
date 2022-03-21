1fb10e21d9976a9c36be3b8d3a2cf71a1472133a
Adrian Brown <adrian.r.brown90@gmail.com>
2022-03-21 10:26:05 -0700

    minor change

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -17,7 +17,7 @@ FROM employees
 WHERE month(birth_date) = 12
   AND day(birth_date) = 25
   AND year(hire_date) BETWEEN 1990 AND 1999
-ORDER BY birth_date, hire_date DESC;
+ORDER BY hire_date DESC;
 
 
 SELECT DATEDIFF(NOW(), hire_date)
