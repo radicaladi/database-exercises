9a19fd9dde9b0ef223bc0d2a7479a4d16aab79fe
Adrian Brown <adrian.r.brown90@gmail.com>
2022-03-21 10:25:19 -0700

    refactor exercises for readability

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,36 +1,27 @@
 USE employees;
 
-SELECT first_name, last_name, gender
+SELECT CONCAT(first_name, ' ', last_name)
 FROM employees
-WHERE first_name IN ('Irena', 'Vidya')
-   OR first_name = 'Maya'
-    AND gender = 'male'
-ORDER BY last_name DESC, first_name DESC;
+WHERE last_name LIKE 'e%e';
 
-SELECT last_name, emp_no
-FROM employees
-WHERE last_name LIKE 'e%'
-   OR last_name LIKE '%e'
-ORDER BY emp_no DESC;
-
-SELECT CONCAT(last_name, ' ', first_name)
-FROM employees
-WHERE last_name LIKE 'e%'
-  AND last_name LIKE '%e';
-
-SELECT last_name
-FROM employees
-WHERE last_name LIKE '%q%'
-  AND last_name NOT LIKE '%qu%';
 
 SELECT *
 FROM employees
 WHERE month(birth_date) = 12
-  AND day(birth_date) = 25;
+  AND day(birth_date) = 25
+  AND year(hire_date) BETWEEN 1990 AND 1999;
 
-SELECT DATEDIFF(now(), hire_date)
+
+SELECT *
 FROM employees
 WHERE month(birth_date) = 12
-AND day(birth_date) = 25
-AND year(hire_date) BETWEEN 1990 AND 1999
-ORDER BY hire_date DESC;
+  AND day(birth_date) = 25
+  AND year(hire_date) BETWEEN 1990 AND 1999
+ORDER BY birth_date, hire_date DESC;
+
+
+SELECT DATEDIFF(NOW(), hire_date)
+FROM employees
+WHERE month(birth_date) = 12
+  AND day(birth_date) = 25
+  AND year(hire_date) BETWEEN 1990 AND 1999;
