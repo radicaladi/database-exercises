5e4a04b8523490908096f09725ae7c9ab0d00bda
Adrian Brown <adrian.r.brown90@gmail.com>
2022-03-21 13:34:02 -0700

    add count() to last_name like q, not like qu

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -7,7 +7,7 @@ SELECT DISTINCT first_name, last_name
 FROM employees
 WHERE last_name LIKE 'e%e';
 
-SELECT DISTINCT last_name
+SELECT COUNT(*), last_name
 FROM employees
-WHERE last_name LIKE '%q%'
-  AND last_name NOT LIKE '%qu%';
+WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
