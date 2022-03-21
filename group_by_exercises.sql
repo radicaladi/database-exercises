7f9e3341969c15d2e6ab8aa4c4ce3fbb1490a429
Adrian Brown <adrian.r.brown90@gmail.com>
2022-03-21 13:36:31 -0700

    add count() first_name group by query

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -11,3 +11,8 @@ SELECT COUNT(*), last_name
 FROM employees
 WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%'
 GROUP BY last_name;
+
+SELECT COUNT(*), gender
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+GROUP BY gender;
