2b0685885e9f44766983994ab523c36d04ba7dd0
Adrian Brown <adrian.r.brown90@gmail.com>
2022-03-21 15:51:06 -0700

    add most common from date; add highest employee number engineer

--- extra_exercises.sql
+++ extra_exercises.sql
@@ -13,3 +13,18 @@ FROM titles
 WHERE title = 'Senior Engineer'
 ORDER BY emp_no, from_date;
 #  10001
+
+# what is the most common ‘from date’ for job titles? for staff only?
+SELECT title, from_date, COUNT(from_date)
+FROM titles
+GROUP BY title, from_date
+ORDER BY COUNT(from_date) DESC;
+#  staff = 20 april 1990
+# most common = 25 oct 1998
+
+# what is the highest employee number for an engineer?
+SELECT emp_no
+FROM titles
+WHERE title = 'Engineer'
+ORDER BY emp_no desc;
+# emp_no 499999
