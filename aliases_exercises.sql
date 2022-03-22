3694fefe6cc173bb23b3e8a4ad747748f1475a9b
Adrian Brown <adrian.r.brown90@gmail.com>
2022-03-22 08:08:18 -0700

    add emp_no format to last query

--- aliases_exercises.sql
+++ aliases_exercises.sql
@@ -1,6 +1,7 @@
 USE employees;
 
-SELECT CONCAT(last_name, ', ', first_name) AS full_name, birth_date AS DOB
+SELECT CONCAT(emp_no, ' - ', last_name, ', ', first_name) AS full_name, birth_date AS DOB
 FROM employees
 ORDER BY full_name
 LIMIT 10;
+
