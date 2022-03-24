398b6dcad90781ec7625a47270b7892ea2abb0f1
Adrian Brown <adrian.r.brown90@gmail.com>
2022-03-24 10:00:49 -0700

    add sub-query for all departments with current female manager

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -22,3 +22,15 @@ WHERE emp_no IN (
     FROM dept_manager
     WHERE to_date = '9999-01-01')
   AND gender = 'F';
+
+SELECT dept_name
+FROM departments
+WHERE dept_no IN (
+    SELECT dept_no
+    FROM dept_manager
+    WHERE to_date = '9999-01-01'
+      AND emp_no IN (
+        SELECT emp_no
+        FROM employees
+        WHERE gender = 'F')
+);
