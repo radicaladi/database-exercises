4943401ed7105935dce8ebc8714c92d657f43760
Adrian Brown <adrian.r.brown90@gmail.com>
2022-03-24 09:33:06 -0700

    add sub-query for all titles help by emp with first_name aamod

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -6,3 +6,11 @@ WHERE hire_date IN (
     SELECT hire_date FROM employees
     WHERE hire_date = '1990-10-22'
 );
+
+SELECT *
+FROM titles
+WHERE emp_no IN (
+    SELECT emp_no
+    FROM employees
+    WHERE first_name = 'Aamod'
+);
